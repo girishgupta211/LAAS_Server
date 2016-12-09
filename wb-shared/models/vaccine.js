c195d782670cae796e34aacaa9b432e904aa7181
ggirgup <girishgargdce@gmail.com>
2016-12-09 13:30:38 +0530

    update vaccine model

--- wb-shared/models/vaccine.js
+++ wb-shared/models/vaccine.js
@@ -3,26 +3,12 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema, log = require('../utils/logger').root.child({ 'module': __filename.substring(__dirname.length + 1, __filename.length - 3) }), bcrypt = require('..').utils.crypter, co = require('co'), constants = require('../utils/constants.js');
 
 var VaccineSchema = new Schema(
-    {
-    "_id": {
-        "$oid": {
-        "type": "string"
-        },
-        "type": "object"
-    },
-
-    "Vaccine": {
-    "type": "string"
-    },
-    "Prevention": {
-    "type": "string"
-    },
-    "Time": {
-    "type": "string"
-    },
-    "Description": {
-    "type": "string"
-    }
+  {
+    Vaccine: {  type: String  },
+    Age: { type: String },
+    Days : { type : Number },
+    Prevention: { type: String },
+    Comments: {  type: String  }
   }
 
 , {
